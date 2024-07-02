e42c02e5d32ab96220cff90dfe94e6302cc66187
estevam02009 <manoel.neto53861@alunos.ufersa.edu.br>
2024-07-02 19:58:56 -0300

    Criado controller e rotas admin, user, vendor

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\Backend\AdminController;
+use App\Http\Controllers\Backend\UserController;
+use App\Http\Controllers\Backend\VendorController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -18,3 +21,18 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+// Rota Admin
+Route::get('admin/dashboard', [AdminController::class, 'dashboard'])
+        ->middleware('auth')
+        ->name('admin.dashboard');
+
+// Rota vendor
+Route::get('vendor/dashboard', [VendorController::class, 'dashboard'])
+    ->middleware('auth')
+    ->name('vendor.dashboard');
+
+// Rota user
+Route::get('user/dashboard', [UserController::class, 'dashboard'])
+    ->middleware('auth')
+    ->name('user.dashboard');
